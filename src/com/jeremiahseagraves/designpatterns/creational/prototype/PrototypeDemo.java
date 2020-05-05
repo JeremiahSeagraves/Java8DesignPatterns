986d89ee87cdc3489d718c7589cdc432c20491c7
= <j.m.s.p.jerry@gmail.com>
2020-05-05 13:15:59 -0500

    added timers for prototype

--- src/com/jeremiahseagraves/designpatterns/creational/prototype/PrototypeDemo.java
+++ src/com/jeremiahseagraves/designpatterns/creational/prototype/PrototypeDemo.java
@@ -2,23 +2,37 @@ package com.jeremiahseagraves.designpatterns.creational.prototype;
 
 public class PrototypeDemo {
 
-	public static void main(String[] args) {
-		ItemRegistry itemRegistry = new ItemRegistry();
-		Movie movie = (Movie) itemRegistry.createItem("Movie");//this will give us an instance of movie by cloning the default inside the itemRegistry
-		movie.setTitle("Creational Patterns in Java");
-		
-		System.out.println(movie);
-		System.out.println(movie.getRuntime());
-		System.out.println(movie.getTitle());
-		System.out.println(movie.getUrl());
-		
-		Movie anotherMovie = (Movie) itemRegistry.createItem("Movie");//another unique instance by using the same clone inside the itemRegistry
-		anotherMovie.setTitle("Gang of Four");
-		
-		System.out.println(anotherMovie);
-		System.out.println(anotherMovie.getRuntime());
-		System.out.println(anotherMovie.getTitle());
-		System.out.println(anotherMovie.getUrl());
-	}
+    public static void main(String[] args) {
+        Long beforeTime = System.currentTimeMillis();
+        ItemRegistry itemRegistry = new ItemRegistry();//only this will take some time
+        Long afterTime = System.currentTimeMillis();
+        System.out.println(afterTime - beforeTime);
+
+        beforeTime = System.currentTimeMillis();
+        Movie movie = (Movie) itemRegistry.createItem("Movie");//this will give us an instance of movie by cloning the default inside the itemRegistry
+        // and it will take near or equal 0ms
+        afterTime = System.currentTimeMillis();
+        System.out.println(afterTime - beforeTime);
+
+        movie.setTitle("Creational Patterns in Java");
+
+        System.out.println(movie);
+        System.out.println(movie.getRuntime());
+        System.out.println(movie.getTitle());
+        System.out.println(movie.getUrl());
+
+        beforeTime = System.currentTimeMillis();
+        Movie anotherMovie = (Movie) itemRegistry.createItem("Movie");//another unique instance by using the same clone inside the itemRegistry
+        //it will also take near or equal 0ms
+        afterTime = System.currentTimeMillis();
+        System.out.println(afterTime - beforeTime);
+
+        anotherMovie.setTitle("Gang of Four");
+
+        System.out.println(anotherMovie);
+        System.out.println(anotherMovie.getRuntime());
+        System.out.println(anotherMovie.getTitle());
+        System.out.println(anotherMovie.getUrl());
+    }
 
 }
