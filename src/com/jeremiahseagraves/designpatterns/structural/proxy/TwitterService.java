fd2a3177dca3156afe4e695c30570cc54ebce6aa
= <j.m.s.p.jerry@gmail.com>
2020-05-07 16:58:40 -0500

    added proxy

---
+++ src/com/jeremiahseagraves/designpatterns/structural/proxy/TwitterService.java
@@ -0,0 +1,7 @@
+package com.jeremiahseagraves.designpatterns.structural.proxy;
+
+public interface TwitterService {
+    String getTimeline(String screenName);
+
+    void postToTimeline(String screenName, String message);
+}
